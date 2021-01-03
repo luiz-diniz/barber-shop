c50402c8d1751247acbdeeefef49924a9b76cae8
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-03 11:13:21 -0300

    Payment setup

---
+++ BarberShop.Service/Repository/DataBase/PaymentRepository.cs
@@ -0,0 +1,28 @@
+﻿using BarberShop.Service.Models;
+using BarberShop.Service.Repository.Interfaces.ModelsRepository;
+
+namespace BarberShop.Service.Repository.Database
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        public void Create(Payment payment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Payment name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Payment Read(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Payment payment)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BarberShop.Service/Services/PaymentService.cs
@@ -0,0 +1,28 @@
+﻿using BarberShop.Service.Models;
+using BarberShop.Service.Services.Interfaces;
+
+namespace BarberShop.Service.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public void Create(Payment payment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Payment payment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Payment Read(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Payment payment)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
