bc389d2fd4b7bbebd0e8ecc5c64f5f5f316547c6
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-04 20:47:31 -0300

    ShopAddress model

---
+++ BarberShop.Service/Models/ShopAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberShop.Service.Models
+{
+    public class ShopAddress
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
