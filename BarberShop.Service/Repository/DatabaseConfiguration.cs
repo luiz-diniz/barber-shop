aa4da4520d17e2066c195145b6cb8ba991ddc5d6
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-05 14:05:19 -0300

    Remove sortinhs

--- BarberShop.Service/Repository/DatabaseConfiguration.cs
+++ BarberShop.Service/Repository/DatabaseConfiguration.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BarberShop.Service.Repository
+﻿namespace BarberShop.Service.Repository
 {
     public class DatabaseConfiguration
     {
         public string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BarberShop;Integrated Security=SSPI";
     }
 }
-
