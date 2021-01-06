6f980929ffd384d6ddeaa1fa33ef55a790115eb1
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-06 00:04:16 -0300

    OrderInfo setup

--- BarberShop.Service/Controllers/OrderInfoController.cs
+++ BarberShop.Service/Controllers/OrderInfoController.cs
@@ -1,4 +1,5 @@
-﻿using BarberShop.Service.Services.Interfaces;
+﻿using BarberShop.Service.Models;
+using BarberShop.Service.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarberShop.Service.Controllers
@@ -8,9 +9,38 @@ namespace BarberShop.Service.Controllers
     public class OrderInfoController : Controller
     {
         public IOrderInfoService _orderInfoService { get; set; }
+
         public OrderInfoController(IOrderInfoService orderInfoService)
         {
             _orderInfoService = orderInfoService;
+        }
+
+        [HttpPost]
+        [Route("CreateOrderInfo")]
+        public void Create(OrderInfo orderInfo)
+        {
+            _orderInfoService.Create(orderInfo);
+        }
+
+        [HttpDelete]
+        [Route("DeleteOrderInfo")]
+        public void Delete(OrderInfo orderInfo)
+        {
+            _orderInfoService.Delete(orderInfo);
+        }
+
+        [HttpGet]
+        [Route("ReadOrderInfo")]
+        public void Read(string orderId)
+        {
+            _orderInfoService.Read(orderId);
+        }
+
+        [HttpPut]
+        [Route("UpdateOrderInfo")]
+        public void Update(OrderInfo orderInfo)
+        {
+            _orderInfoService.Update(orderInfo);
         }
     }
 }
