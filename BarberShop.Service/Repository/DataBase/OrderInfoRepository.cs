2e4aaafdbed79a0f14d75a99428430571c0a3a1d
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-05 23:58:52 -0300

    OrderInfo setup

--- BarberShop.Service/Repository/DataBase/OrderInfoRepository.cs
+++ BarberShop.Service/Repository/DataBase/OrderInfoRepository.cs
@@ -9,22 +9,22 @@ namespace BarberShop.Service.Repository.Database
 {
     public class OrderInfoRepository : IOrderInfoRepository
     {
-        public void Create(OrderInfo type)
+        public void Create(OrderInfo orderInfo)
         {
             throw new NotImplementedException();
         }
 
-        public void Delete(OrderInfo type)
+        public void Delete(OrderInfo orderInfo)
         {
             throw new NotImplementedException();
         }
 
-        public OrderInfo Read(string type)
+        public OrderInfo Read(string orderId)
         {
             throw new NotImplementedException();
         }
 
-        public void Update(OrderInfo type)
+        public void Update(OrderInfo orderInfo)
         {
             throw new NotImplementedException();
         }
