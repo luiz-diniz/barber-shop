6812fd2ed52c5a95abd9a04518bef08f6cabe04e
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-07 21:47:45 -0300

    OrderServices setup

--- BarberShop.Service/Controllers/OrderServicesController.cs
+++ BarberShop.Service/Controllers/OrderServicesController.cs
@@ -1,4 +1,6 @@
-﻿using BarberShop.Service.Services.Interfaces;
+﻿using BarberShop.Service.Models;
+using BarberShop.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +8,8 @@ using System.Threading.Tasks;
 
 namespace BarberShop.Service.Controllers
 {
+    [Route("api/orderservices")]
+    [ApiController]
     public class OrderServicesController
     {
         private IOrderServicesService _orderServicesService;
@@ -13,6 +17,30 @@ namespace BarberShop.Service.Controllers
         public OrderServicesController(IOrderServicesService orderServicesService)
         {
             _orderServicesService = orderServicesService;
+        }
+
+        [HttpPost]
+        [Route("CreateOrderServices")]
+        public void Create(OrderServices orderServices)
+        {
+        }
+
+        [HttpDelete]
+        [Route("DeleteOrderServices")]
+        public void Delete(OrderServices orderServices)
+        {
+        }
+
+        [HttpGet]
+        [Route("ReadOrderServices/{id}]")]
+        public OrderServices Read(string id)
+        {
+        }
+
+        [HttpPut]
+        [Route("UpdateOrderServices")]
+        public void Update(OrderServices orderServices)
+        {
         }
     }
 }
