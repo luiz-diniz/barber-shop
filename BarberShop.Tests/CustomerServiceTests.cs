b23fb7868877773504a0fd18a300f0c018710263
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-15 10:04:11 -0300

    CustomerService test setup

--- BarberShop.Tests/CustomerServiceTests.cs
+++ BarberShop.Tests/CustomerServiceTests.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using BarberShop.Service.Repository.Interfaces;
+using BarberShop.Service.Services;
+using Moq;
+using System;
 
 namespace BarberShop.Tests
 {
     public class CustomerServiceTests
     {
+        private Mock<ICustomerRepository> _customerRepository;
+        private Mock<ILogger> _logger;
+
+        public CustomerServiceTests()
+        {
+            _customerRepository = new Mock<ICustomerRepository>();
+            _logger = new Mock<ILogger>();
+        }
+
+        public CustomerService GetInstance()
+        {
+            return new CustomerService(_customerRepository.Object, _logger.Object);
+        }
     }
 }
