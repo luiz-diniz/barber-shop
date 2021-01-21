1f3a996353f80eb9995cd431f65bea81f2e3d079
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-01-21 00:06:28 -0300

    EmployeeTests setup

---
+++ BarberShop.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BarberShop.Service.Repository.Interfaces;
+using BarberShop.Service.Repository.Interfaces.ModelsRepository;
+using BarberShop.Service.Services;
+using BarberShop.Service.Utilities;
+using Moq;
+using Xunit;
+
+namespace BarberShop.Tests
+{
+    public class EmployeeServiceTests
+    {
+        private Mock<IEmployeeRepository> _employeeRepository;
+        private Mock<ILogger> _logger;
+        private Mock<IHasher> _hasher;
+
+        public EmployeeServiceTests()
+        {
+            _employeeRepository = new Mock<IEmployeeRepository>();
+            _logger = new Mock<ILogger>();
+        }
+
+        
+
+        private EmployeeService GetInstance()
+        {
+            return new EmployeeService(_employeeRepository.Object, _logger.Object, _hasher.Object);
+        }
+    }
+}
