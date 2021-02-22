f365ffb9a567937cda8f3c1f2dd7473a71f5c130
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-02-22 16:57:12 -0300

    - ServiceInfo test setup

---
+++ BarberShop.Tests/ServiceInfoServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using BarberShop.Service.Models;
+using BarberShop.Service.Repository.Interfaces;
+using BarberShop.Service.Repository.Interfaces.ModelsRepository;
+using BarberShop.Service.Services;
+using BarberShop.Service.Services.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarberShop.Tests
+{
+    public class ServiceInfoServiceTests
+    {
+        private Mock<IServiceInfoRepository> _ServiceInfoService;
+        private Mock<ILogger> _logger;
+
+        public ServiceInfoServiceTests()
+        {
+            _ServiceInfoService = new Mock<IServiceInfoRepository>();
+            _logger = new Mock<ILogger>();
+        }
+
+        private ServiceInfo GetInstance()
+        {
+            return new ServiceInfoService(_ServiceInfoService.Object, _logger.Object);
+        }
+    }
+}
