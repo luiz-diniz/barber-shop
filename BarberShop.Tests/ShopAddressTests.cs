92473967f842738f3101b3216d1a61bd24b4971d
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-02-25 11:11:35 -0300

    - ShopAddress tests setup

---
+++ BarberShop.Tests/ShopAddressTests.cs
@@ -0,0 +1,28 @@
+﻿using BarberShop.Service.Repository.Interfaces;
+using BarberShop.Service.Repository.Interfaces.ModelsRepository;
+using BarberShop.Service.Services;
+using Moq;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarberShop.Tests
+{
+    public class ShopAddressTests
+    {
+        private Mock<IShopAddressRepository> _shopAddressRepository;
+        private Mock<ILogger> _logger;
+
+        public ShopAddressTests()
+        {
+            _shopAddressRepository = new Mock<IShopAddressRepository>();
+            _logger = new Mock<ILogger>();
+        }
+
+        public ShopAddressService GetInstance()
+        {
+            return new ShopAddressService(_shopAddressRepository.Object, _logger.Object);
+        }
+    }
+}
