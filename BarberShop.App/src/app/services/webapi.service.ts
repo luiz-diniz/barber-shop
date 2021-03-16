5e4e37b3b9869a7fcadcde55ed2c7cbeec4098a4
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-03-16 14:14:24 -0300

    - Generic WebApiService and Environment config

---
+++ BarberShop.App/src/app/services/webapi.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CATCH_ERROR_VAR } from '@angular/compiler/src/output/output_ast';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WebapiService<T, Y>{
+
+  constructor(private http: HttpClient) { }
+
+  Create(type: T, api: string) : Observable<Y>{
+    return this.http.post<Y>(environment.url + api, type, httpOptions);
+  }
+
+  Delete(type: T, api: string) : Observable<Y>{
+    const httpDelete = {
+      headers: httpOptions.headers,
+      body: type 
+    }
+
+    return this.http.delete<Y>(environment.url + api, httpDelete);
+  }
+
+  Edit(type: T, api: string) : Observable<Y>{
+    return this.http.put<Y>(environment.url + api, type, httpOptions);
+  }
+
+  GetAllEmployees(api: string) : Observable<Y[]>{
+    return this.http.get<Y[]>(environment.url + api, httpOptions);
+  }
+}
