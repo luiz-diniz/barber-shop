3088cefcd105abe0f7db78c269d75d8148113f66
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-03-28 23:04:36 -0300

    - New phone format support added

--- BarberShop.App/src/app/services/format.service.ts
+++ BarberShop.App/src/app/services/format.service.ts
@@ -10,4 +10,8 @@ export class FormatService {
   FormatCPF(cpf: string){
     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
   }
+
+  FormatPhone(phone: string){
+    return phone.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+  }
 }
