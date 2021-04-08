7f57ec0707f79890fafd08c27ebdb2b77a4394f4
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-04-08 13:28:27 -0300

    - Sql file to populate some tables

---
+++ ConfigurationFiles/SQL/Data.sql
@@ -0,0 +1,5 @@
+insert into ShopAddress values('JohnBC', 'Saint John Av.', '150', 'Vanvouver', 'BC')
+insert into ShopAddress values('JohnNB', 'Eric St.', '84', 'Moncton', 'NB')
+insert into ShopAddress values('JohnAB', 'Vicent St.', '1024', 'Calgary', 'AB')
+insert into ShopAddress values('JohnSK', 'Famous St.', '14', 'Saskatoon', 'SK')
+insert into ShopAddress values('JohnMB', 'George Av.', '1024', 'Winnipeg', 'MB')
