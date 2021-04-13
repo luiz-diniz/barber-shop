4fc37e4748943861dff237f47c206bd8976f7727
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-04-12 22:30:38 -0300

    - Model update

--- BarberShop.App/src/app/models/OrderInfo.ts
+++ BarberShop.App/src/app/models/OrderInfo.ts
@@ -2,11 +2,11 @@ import { Customer } from "./customer";
 import { Employee } from "./employee";
 import { ShopAddress } from "./ShopAddress";
 
-
 export class OrderInfo{
-    public Id: number;
-    public CustomerInfo: Customer;
-    public EmployeeInfo: Employee;
-    public ShopAddressInfo: ShopAddress;
-    public OrderDate: Date;
+    id: number;
+    customerInfo: Customer;
+    employeeInfo: Employee;
+    shopAddressInfo: ShopAddress;
+    orderDate: Date;
+    hide: boolean;
 }
