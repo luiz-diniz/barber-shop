3c463362d23583617817baeb0c47c23be6139c33
Luiz Diniz <luiz.gustavon@hotmail.com>
2021-04-19 22:53:37 -0300

    - OrderInfo component setup

--- BarberShop.App/src/app/components/order-info/order-info.component.ts
+++ BarberShop.App/src/app/components/order-info/order-info.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/models/Customer';
+import { Employee } from 'src/app/models/Employee';
 import { OrderInfo } from 'src/app/models/OrderInfo';
+import { ShopAddress } from 'src/app/models/ShopAddress';
+import { FormatService } from 'src/app/services/format.service';
 import { WebapiService } from 'src/app/services/webapi.service';
 
 @Component({
@@ -9,15 +13,46 @@ import { WebapiService } from 'src/app/services/webapi.service';
 })
 export class OrderInfoComponent implements OnInit {
 
-  constructor(private service: WebapiService<OrderInfo, any>) { }
-  
-  
+  orderInfo: OrderInfo;
+  orders: OrderInfo[];
 
+  addresses: ShopAddress[];
+
+  showForm: boolean = false;
+  isEditing: boolean = false;
+
+  constructor(private service: WebapiService<OrderInfo, any>,
+    private format: FormatService) {
+
+      this.orderInfo = new OrderInfo();
+      this.orderInfo.customerInfo = new Customer();
+      this.orderInfo.employeeInfo = new Employee();
+      this.orderInfo.shopAddressInfo = new ShopAddress();
+
+      this.orders = [];
+      this.addresses = [];
+
+      this.GetAllAddresses();
+     }
+  
   ngOnInit(): void {
-
   }
 
-  GetAllOrders(){
+  OnSubmit(){
+    console.log(0);
+  }
 
+  GetAllAddresses(){
+    const api = "api/shopAddress/GetAllShopAddresses";
+    this.service.GetAll(api).subscribe(
+      shops => {
+        this.addresses = shops;
+        console.log(this.addresses);
+      }, 
+      err =>{
+        console.log(err);
+        alert('Error: Contact the administrator.')
+      }
+    )
   }
 }
